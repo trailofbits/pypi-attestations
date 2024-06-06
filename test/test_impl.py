40c46f86de363263f52fa00f167d2c70a61247ec
William Woodruff <william@trailofbits.com>
2024-06-06 15:52:10 -0400

    test_impl: more cov
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- test/test_impl.py
+++ test/test_impl.py
@@ -3,8 +3,10 @@
 import os
 from pathlib import Path
 
+import pretend
 import pypi_attestation_models._impl as impl
 import pytest
+from sigstore.dsse import _DigestSet, _StatementBuilder, _Subject
 from sigstore.oidc import IdentityToken
 from sigstore.sign import SigningContext
 from sigstore.verify import Verifier, policy
@@ -69,6 +71,24 @@ class TestAttestation:
         ):
             attestation.verify(verifier, pol, modified_artifact_path)
 
+    def test_verify_filename_mismatch(self, tmp_path: Path) -> None:
+        verifier = Verifier.staging()
+        # Our checked-in asset has this identity.
+        pol = policy.Identity(
+            identity="william@yossarian.net", issuer="https://github.com/login/oauth"
+        )
+
+        attestation = impl.Attestation.model_validate_json(attestation_path.read_text())
+
+        modified_artifact_path = tmp_path / "wrong_name-0.1.2-py3-none-any.whl"
+        modified_artifact_path.write_bytes(artifact_path.read_bytes())
+
+        # attestation has the correct digest, but a mismatching filename.
+        with pytest.raises(
+            impl.VerificationError, match="subject does not match distribution name"
+        ):
+            attestation.verify(verifier, pol, modified_artifact_path)
+
     def test_verify_policy_mismatch(self) -> None:
         verifier = Verifier.staging()
         # Wrong identity.
@@ -77,4 +97,54 @@ class TestAttestation:
         attestation = impl.Attestation.model_validate_json(attestation_path.read_text())
 
         with pytest.raises(impl.VerificationError, match=r"Certificate's SANs do not match"):
+            attestation.verify(verifier, pol, artifact_path)
+
+    def test_verify_wrong_envelope(self) -> None:
+        verifier = pretend.stub(
+            verify_dsse=pretend.call_recorder(lambda bundle, policy: ("fake-type", None))
+        )
+        pol = pretend.stub()
+
+        attestation = impl.Attestation.model_validate_json(attestation_path.read_text())
+
+        with pytest.raises(impl.VerificationError, match="expected JSON envelope, got fake-type"):
+            attestation.verify(verifier, pol, artifact_path)
+
+    def test_verify_bad_payload(self) -> None:
+        verifier = pretend.stub(
+            verify_dsse=pretend.call_recorder(
+                lambda bundle, policy: ("application/vnd.in-toto+json", b"invalid json")
+            )
+        )
+        pol = pretend.stub()
+
+        attestation = impl.Attestation.model_validate_json(attestation_path.read_text())
+
+        with pytest.raises(impl.VerificationError, match="invalid statement"):
+            attestation.verify(verifier, pol, artifact_path)
+
+    def test_verify_too_many_subjects(self) -> None:
+        statement = (
+            _StatementBuilder()  # noqa: SLF001
+            .subjects(
+                [
+                    _Subject(name="foo", digest=_DigestSet(root={"sha256": "abcd"})),
+                    _Subject(name="bar", digest=_DigestSet(root={"sha256": "1234"})),
+                ]
+            )
+            .predicate_type("foo")
+            .build()
+            ._inner.model_dump_json()
+        )
+
+        verifier = pretend.stub(
+            verify_dsse=pretend.call_recorder(
+                lambda bundle, policy: ("application/vnd.in-toto+json", statement.encode())
+            )
+        )
+        pol = pretend.stub()
+
+        attestation = impl.Attestation.model_validate_json(attestation_path.read_text())
+
+        with pytest.raises(impl.VerificationError, match="too many subjects in statement"):
             attestation.verify(verifier, pol, artifact_path)
