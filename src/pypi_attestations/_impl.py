f3a412c978aa899245e9f9524645926483394a2c
William Woodruff <william@trailofbits.com>
2024-07-10 13:59:18 -0400

    Add `AttestationType`, check during verify (#29)
    * _impl: enumerate known attestation types
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * _impl: reject unknown attestations during verify
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * fix online markers, add coverage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * pypi_attestations: avoid StrEnum
    
    Not available before 3.11.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * _impl: fix predicate_type
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/pypi_attestations/_impl.py
+++ src/pypi_attestations/_impl.py
@@ -6,6 +6,7 @@ This module is NOT a public API, and is not considered stable.
 from __future__ import annotations
 
 import base64
+from enum import Enum
 from typing import TYPE_CHECKING, Annotated, Any, Literal, NewType
 
 import sigstore.errors
@@ -35,6 +36,13 @@ if TYPE_CHECKING:
     from sigstore.sign import Signer  # pragma: no cover
     from sigstore.verify import Verifier  # pragma: no cover
     from sigstore.verify.policy import VerificationPolicy  # pragma: no cover
+
+
+class AttestationType(str, Enum):
+    """Attestation types known to PyPI."""
+
+    SLSA_PROVENANCE_V1 = "https://slsa.dev/provenance/v1"
+    PYPI_PUBLISH_V1 = "https://docs.pypi.org/attestations/publish/v1"
 
 
 class AttestationError(ValueError):
@@ -119,7 +127,7 @@ class Attestation(BaseModel):
                         )
                     ]
                 )
-                .predicate_type("https://docs.pypi.org/attestations/publish/v1")
+                .predicate_type(AttestationType.PYPI_PUBLISH_V1)
                 .build()
             )
         except DsseError as e:
@@ -185,6 +193,11 @@ class Attestation(BaseModel):
         digest = subject.digest.root.get("sha256")
         if digest is None or digest != expected_digest:
             raise VerificationError("subject does not match distribution digest")
+
+        try:
+            AttestationType(statement.predicate_type)
+        except ValueError:
+            raise VerificationError(f"unknown attestation type: {statement.predicate_type}")
 
         return statement.predicate_type, statement.predicate
 
