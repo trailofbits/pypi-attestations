ffd5bb461136d2d8b92b8708eda8569e6e9048ee
William Woodruff <william@astral.sh>
2025-07-23 13:15:52 -0400

    conftest: give EXTREMELY_DANGEROUS_PUBLIC_OIDC_BEACON precedence

--- test/conftest.py
+++ test/conftest.py
@@ -6,12 +6,7 @@ from sigstore import oidc
 
 @pytest.fixture(scope="session")
 def id_token() -> oidc.IdentityToken:
-    if "CI" in os.environ:
-        token = oidc.detect_credential()
-        if token is None:
-            pytest.fail("misconfigured CI: no ambient OIDC credential")
-        return oidc.IdentityToken(token)
-    elif "EXTREMELY_DANGEROUS_PUBLIC_OIDC_BEACON" in os.environ:
+    if "EXTREMELY_DANGEROUS_PUBLIC_OIDC_BEACON" in os.environ:
         import requests
 
         resp = requests.get(
@@ -20,5 +15,11 @@ def id_token() -> oidc.IdentityToken:
         resp.raise_for_status()
         id_token = resp.text.strip()
         return oidc.IdentityToken(id_token)
+
+    if "CI" in os.environ:
+        token = oidc.detect_credential()
+        if token is None:
+            pytest.fail("misconfigured CI: no ambient OIDC credential")
+        return oidc.IdentityToken(token)
     else:
         return oidc.Issuer("https://oauth2.sigstage.dev/auth").identity_token()
